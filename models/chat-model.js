29bc3f567524ea6a1d184aa461b41461364825b0
Sladrus <ufimkinyasha@gmail.com>
2023-05-10 19:32:59 +0300

    fix models

--- models/chat-model.js
+++ models/chat-model.js
@@ -1,41 +1,39 @@
 const Sequelize = require('sequelize');
-module.exports = function(sequelize, DataTypes) {
-  return sequelize.define('Chats', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    group_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true
-    },
-    chat_url: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    active: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      defaultValue: 0
-    },
-    chat_id: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    }
-  }, {
-    sequelize,
-    tableName: 'chats',
-    timestamps: false,
-    indexes: [
-      {
-        name: "id",
-        using: "BTREE",
-        fields: [
-          { name: "id" },
-        ]
+module.exports = function (sequelize, DataTypes) {
+  return sequelize.define(
+    'Chats',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
       },
-    ]
-  });
+      chat_url: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+      active: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      chat_id: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      tableName: 'chats',
+      timestamps: false,
+      indexes: [
+        {
+          name: 'id',
+          using: 'BTREE',
+          fields: [{ name: 'id' }],
+        },
+      ],
+    }
+  );
 };
